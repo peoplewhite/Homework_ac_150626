624b4fe188b835b1c24c09fde72b0ddcabeced38
peoplewhite <shonshon7@gmail.com>
2015-06-25 16:27:11 +0800

    add user

--- Homework_ac_150625_1/ViewController.m
+++ Homework_ac_150625_1/ViewController.m
@@ -17,20 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    PFUser *user = [PFUser user];
-    user.username = @"sean";
-    user.password = @"111";
-    user.email = @"shonshon7@gmail.com";
-    
-    // other fields can be set just like with PFObject
-    user[@"phone"] = @"0932453801";
-    
-    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-        if (!error) {   // Hooray! Let them use the app now.
-            NSLog(@"Done");
-        } else {   NSString *errorString = [error userInfo][@"error"];   // Show the errorString somewhere and let the user try again.
-        }
-    }];
     
 }
 
