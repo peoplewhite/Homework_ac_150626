6c200d709dc224cb4744c6ea85b22bcc14da861e
peoplewhite <shonshon7@gmail.com>
2015-06-25 23:12:39 +0800

    add singleton done

---
+++ Homework_ac_150625_1/SingletonObject.h
@@ -0,0 +1,13 @@
+//
+//  SingletonObject.h
+//  Homework_ac_150625_1
+//
+//  Created by Po-Hao Cheng on 2015/6/25.
+//  Copyright (c) 2015年 phc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SingletonObject : NSObject
+
+@end
---
+++ Homework_ac_150625_1/SingletonObject.m
@@ -0,0 +1,13 @@
+//
+//  SingletonObject.m
+//  Homework_ac_150625_1
+//
+//  Created by Po-Hao Cheng on 2015/6/25.
+//  Copyright (c) 2015年 phc. All rights reserved.
+//
+
+#import "SingletonObject.h"
+
+@implementation SingletonObject
+
+@end
